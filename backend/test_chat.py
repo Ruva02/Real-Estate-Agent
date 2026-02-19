59326c5ca0fa83072a8924fa7aec3a99a23b16b4
Rudra Sthapak <sthapakram02@gmail.com>
2026-02-19 13:58:56 +0530

    Delete backend directory

--- backend/test_chat.py
+++
@@ -1,7 +0,0 @@
-import requests
-try:
-    response = requests.post("http://localhost:8002/chat", json={"message": "Hello"})
-    print(response.status_code)
-    print(response.text)
-except Exception as e:
-    print(e)
